4f99d12800dc9007a697333381cf75baf2f6819d
장두혁 <jangduhyeog@jangduhyeog-ui-MacBookPro.local>
2022-10-06 17:36:03 +0900

    1006

--- todos.js
+++ todos.js
@@ -6,6 +6,8 @@ let id = 0;
 
 
 // 할일 추가하기
+
+// todos 초기화
 const setTodos = (newTodos) => {
     todos = newTodos;
 }
@@ -14,6 +16,7 @@ const getAllTodos =  () => {
     return todos;
 }
 
+// todos 렌더링함수
 const appendTodos = (text) => {
     const newId = id++;
     /* concat(): 배열합치기
@@ -31,13 +34,59 @@ const appendTodos = (text) => {
 }
 
 
-// html에 추가된 할 일 그리기
+// 삭제 이벤트 처리 함수
+const deleteTodo = (todoId) => {
+    // filter() 배열에서 조건에 맞춰 찾기
+    const newTodos = getAllTodos().filter(todo => todo.id !== todoId);
+    setTodos(newTodos);
+    paintTodos();   // 삭제된 배열을 다시 렌더링
+}
+
+
+// 완료 처리 함수
+const completeTodo = (todoId) => {
+    // 삼항연산자 (조건문) ? true : false; 
+    // true면, ...todo에 isCompleted: ! todo.isCompleted 반환
+    const newTodos = getAllTodos().map(
+        todo => todo.id === todoId ? {...todo, isCompleted: !todo.isCompleted} : todo)
+    setTodos(newTodos);
+    paintTodos();
+}
+
+
+// 수정
+const onDbclickTodo = (e, todoId) => {
+    const todoElem = e.target;
+    const inputText = e.target.innerText;
+    const todoItemElem = todoElem.parentNode;
+    const inputElem = document.createElement('input');
+    inputElem.value = inputText;
+    // input이 원래 value를 가려지게 디자인
+    inputElem.classList.add('edit-input');
+    todoItemElem.appendChild(inputElem);
+
+    inputElem.addEventListener('keypress', (e) =>{
+        if(e.key === 'Enter'){
+            updateTodo(e.target.value, todoId);
+        }
+    })
+}
+
+const updateTodo = (text, todoId) => {
+    const newTodos = getAllTodos().map(todo => todoId === todo.id ? ({...todo, content: text}) : todo);
+    setTodos(newTodos);
+    paintTodos();
+}
+
+
+
 const paintTodos = () => {
+    // html에 추가된 할 일 그리기
     todoListElem.innerHTML = null; // todoListElem html 초기화
     const allTodos = getAllTodos(); // todos배열
 
     // "todo-item"에 해당 html을 그려 리스트에 추가
-    /*          <li class="todo-item checked">
+    /*          <li class="todo-item checked" data-id="n">
                     <div class="checkbox">✔</div>
                     <div class="todo">todo.content</div>
                     <button class="delBtn">X</button>
@@ -48,15 +97,22 @@ const paintTodos = () => {
         const todoItemElem = document.createElement('li');
         todoItemElem.classList.add('todo-item');
 
+        todoItemElem.setAttribute('data-id', todo.id);
+
         const checkboxElem = document.createElement('div');
         checkboxElem.classList.add('checkbox');
+        checkboxElem.addEventListener('click', () => completeTodo(todo.id));
 
         const todoElem = document.createElement('div');
         todoElem.classList.add('todo');
+        // 더블클릭 이벤트
+        todoElem.addEventListener('dblclick', (event) => onDbclickTodo(event, todo.id))
         todoElem.innerText = todo.content;
 
         const delBtnElem = document.createElement('button');
         delBtnElem.classList.add('delBtn');
+        // 할일 목록 삭제 이벤트 생성
+        delBtnElem.addEventListener('click', () => deleteTodo(todo.id));
         delBtnElem.innerText = "X";
 
         if(todo.isCompleted){
@@ -74,11 +130,11 @@ const paintTodos = () => {
 
 
 
+
 // keypress(입력)에 대한 이벤트 리스너 등록
 //todos.js 실행시 바로 실행하는 함수
-
 const init = () => {
-    todoInputElem.addEventListener('keypress', (e) =>{
+    todoInputElem.addEventListener('keypress', (e) => {
         if( e.key === 'Enter' ){
             appendTodos(e.target.value); e.target.value ='';
             //todoInputElem.value = ''; 할때 text칸이 비워지지 않음
